33943745b67f522d0a81bce5b00e414eb48516d0
Ben Miles <bcgm3studios@gmail.com>
2021-12-30 13:07:07 -0500

    Initial Commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<title>Jasmine Slater</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Jasmine Slater's photo blog">
+		<meta name="author" content="Jasmine Slater">
+		<!-- <meta property="og:title" content="A Basic HTML5 Template">
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://www.sitepoint.com/a-basic-html5-template/">
+		<meta property="og:description" content="A simple HTML5 Template for new projects.">
+		<meta property="og:image" content="image.png"> -->
+		<!-- <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.js'; ?>"></script> -->
+		<!-- <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-ui.min.js'; ?>"></script> -->
+		<!-- <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>"></script> -->
+		<!-- <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>"> -->
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+		<!-- <link rel="icon" href="/favicon.ico"> -->
+		<!-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> -->
+		<!-- <link rel="apple-touch-icon" href="/apple-touch-icon.png"> -->
+
+	</head>
+
+	<body>
+
+		<div id="ttr_header" class="jumbotron">
+			<h1>HEADER</h1>
+		</div>
+		<div class="container">
